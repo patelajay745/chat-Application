77bf2937e9d9a2ef06db6f8f36917f0a0aba4357
Ajay patel <patel.ajay745@gmail.com>
2024-12-02 21:55:38 -0500

    added cicd for frontend

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -26,7 +26,6 @@ function App() {
   const wsRef = useRef<WebSocket | null>(null);
 
   useEffect(() => {
-    // backend url
     const ws = new WebSocket(import.meta.env.VITE_BACKEND_URL);
 
     wsRef.current = ws;
